8a102aff399f5fc152d1a9530b59f4bd3fc1d6e4
DARSHAN K <121747344+KDSCRIPT@users.noreply.github.com>
2023-09-15 09:19:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from amazon_bot import chat_with_bot
 import joblib
-ml_model=joblib.load("LinearSVC.pkl")
+from sklearn.feature_extraction.text import TfidfVectorizer
+ml_model=joblib.load("model.pkl")
 st.title('Amazon Chatbot')
 
 user_input=st.sidebar.text_input("Enter your inquiry:")
@@ -11,7 +12,9 @@ if st.sidebar.button("Submit"):
         st.sidebar.text("Chatbot: Goodbye!")
     else:
         bot_response=chat_with_bot(user_input)
-        ml_prediction=ml_model.predict([user_input])
+        tfidf_vectorizer = TfidfVectorizer()
+        user_input_tfidf = tfidf_vectorizer.transform([user_input]) 
+        ml_prediction=ml_model.predict(user_input_tfidf)
         st.write("Amazon bot response:")
         st.write(bot_response)
         st.write("ML Model Prediction:")
